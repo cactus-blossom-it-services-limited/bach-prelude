69e700a40e30749d1ab53a020191f596e885b15a
Andrew Farquharson <andrew.farquharson@gmail.com>
2021-02-13 16:53:07 +0000

    composer files, custom mod, dev services yaml

--- web/modules/custom/dino_roar/src/Controller/RoarController.php
+++ web/modules/custom/dino_roar/src/Controller/RoarController.php
@@ -1,1 +1,13 @@
 <?php
+
+namespace Drupal\dino_roar\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class RoarController
+{
+  public function roar()
+  {
+    return new Response('ROOOOOOOOAR!');
+  }
+}
